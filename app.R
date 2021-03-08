20e4dbce49f0ada7f8c87103c76766a7addb769b
Molo <molo@Molos-MacBook-Air.local>
2021-03-08 20:23:38 +0300

    added global.R and added visuals

--- app.R
+++ app.R
@@ -6,27 +6,9 @@
  
 library(easypackages)
 libraries("shiny","shinydashboard","tidyverse","lubridate", "plotly")
+theme_set(theme_minimal())
 
-# Dataset
-medicalPractitioners <- read.csv("Data/MedicalPractitioners.csv")
-medicalPractitioners$RegDate <- dmy(medicalPractitioners$RegDate)
-medicalPractitioners <- medicalPractitioners %>%
-    select(RegDate, SPECIALTY, SUB_SPECIALTY, Qualification.Count, TOWN) %>%
-    rename(
-           `Registration Date` = RegDate,
-           Specialty = SPECIALTY,
-           `Sub Specialty` = SUB_SPECIALTY,
-           `Number of Qualifications` = Qualification.Count,
-           Town = TOWN) %>%
-    dplyr::mutate(Year = lubridate::year(`Registration Date`))
-
-medicalPractitioners$`Year Range` = cut(medicalPractitioners$Year, c(1970, 1980, 1990, 2000, 2010, 2020, 2025))
-levels(medicalPractitioners$`Year Range`) = c("1970-1980", "1981-1990", "1991-2000","2001-2010", "2011-2020","2021")
-medicalPractitioners$`Year Range` <- factor(medicalPractitioners$`Year Range`, ordered = T, levels = c("1970-1980", "1981-1990", "1991-2000","2001-2010", "2011-2020"))
-
-
-set.seed(1234)
-
+town <- unique(medicalPractitioners$Town)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -37,7 +19,7 @@ ui <- fluidPage(
         ),
         dashboardSidebar(
             sidebarMenu(
-                sidebarSearchForm(textId = "SearchTown", buttonId = "searchButton",
+                sidebarSearchForm(textId = "Search", buttonId = "searchTown",
                                   label = "Search Town")
             )
         ),
@@ -45,9 +27,8 @@ ui <- fluidPage(
             fluidRow(
                 tabBox(width = 12, height = NULL, selected = "Count of Practitioners",
                        tabPanel("Count of Practitioners", plotlyOutput("practitioners_count")),
-                       tabPanel("Count of Qualifications", plotlyOutput("qualifications_count")),
-                       tabPanel("Specialty of Practitioners", plotlyOutput("specialityCount")),
-                       tabPanel("Sub Speciality of Practitioners", plotlyOutput("subSpecialtyCount"))
+                       tabPanel("Number of Qualifications per Medical Practitioners", plotlyOutput("qualifications_count")),
+                       tabPanel("Top Specialties of Medical Practitioners", plotlyOutput("specialityCount"))
                        )
             )
         ) # End of dashboardBody()
@@ -57,15 +38,87 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    # Server Output - Count of Practitioners ####
+    output$practitioners_count <- renderPlotly({
+        medicalPractitioners %>%
+            group_by(`Year Range`) %>%
+            summarise(count = n()) %>%
+            ggplot(aes(`Year Range`, count)) +
+            geom_line(aes(group = 1),color="#aa2b1d", size=1) + 
+            geom_point(size=4, color="#28527a") + 
+            labs(
+                 subtitle = "Data from 1978 to 2020",
+                 caption = "Source: medicalboard.co.ke",
+                 x="") +
+            theme(
+                # plot.title = element_text(color = "#23689b", size = 13, face = "bold",hjust = 0.5),
+                plot.subtitle = element_text(color = "#161d6f", size = 9, face = "bold",hjust = 0.5),
+                plot.caption = element_text(color = "#0f1123", size = 10, face = "italic"),
+                axis.text.x = element_text(face = "bold", size = 8),
+                axis.text.y = element_text(face = "bold", size = 8)
+            ) + 
+            geom_label(aes(label=count),
+                       nudge_x = 0.1,
+                       nudge_y = 0.2,
+                       size=5)
     })
+    
+    # Server Output - Count of Qualifications ####
+    output$qualifications_count <- renderPlotly({
+        medicalPractitioners %>%
+            group_by(`Year Range`, `Number of Qualifications`) %>%
+            summarise(count = n()) %>%
+            ggplot(aes(`Year Range`, count, group=`Number of Qualifications`)) +
+            geom_line(aes(color=`Number of Qualifications`), size=1) + 
+            geom_point(aes(color=`Number of Qualifications`), size=3) + 
+            labs(
+                 subtitle = "Data from 1978 to 2020",
+                 caption = "Source:https://medicalboard.co.ke/DashBoard.php ",
+                 x="") +
+            theme(
+                # plot.title = element_text(color = "#23689b", size = 20, face = "bold",hjust = 0.5),
+                plot.subtitle = element_text(color = "#161d6f", size = 13, face = "bold",hjust = 0.5),
+                plot.caption = element_text(color = "#0f1123", size = 10, face = "italic"),
+                axis.text.x = element_text(face = "bold", size = 8),
+                axis.text.y = element_text(face = "bold", size = 8),
+                legend.title = element_blank(),
+                legend.position = "top"
+            ) + 
+            geom_label(aes(label=count),
+                       nudge_x = 0.15,
+                       nudge_y = 4,
+                       size=2)
+    })
+    
+    # Server Output - Top Specialties ####
+    output$specialityCount <- renderPlotly({
+        medicalPractitioners  %>% 
+            count(Specialty, sort = T) %>%
+            filter(n>20) %>%
+            ggplot(aes(reorder(Specialty, n), n)) + 
+            geom_col(aes(fill=Specialty)) + 
+            coord_flip() + 
+            labs(
+                 subtitle = "Data from 1978 to 2020",
+                 caption = "Source:https://medicalboard.co.ke/DashBoard.php ",
+                 x="", y="count") +
+            theme(
+                # plot.title = element_text(color = "#23689b", size = 20, face = "bold",hjust = 0.5),
+                plot.subtitle = element_text(color = "#161d6f", size = 13, face = "bold",hjust = 0.5),
+                plot.caption = element_text(color = "#0f1123", size = 10, face = "italic"),
+                axis.text.x = element_text(face = "bold", size = 8),
+                axis.text.y = element_text(face = "bold", size = 8),
+                legend.title = element_blank(),
+                legend.position = "none"
+            ) + 
+            geom_label(aes(label=n),
+                       nudge_x = 0.15,
+                       nudge_y = 0.9,
+                       size=2)
+    })
+    
+
 }
 
 # Run the application 
